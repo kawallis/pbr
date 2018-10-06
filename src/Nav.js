f1ef6b9558c2078672dbafad8621f6d90a481f98
cwally <kawallis@icloud.com>
2018-10-06 16:06:18 -0700

    added stuff

--- src/Nav.js
+++ src/Nav.js
@@ -1,41 +1,43 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { FaTachometerAlt, FaMoneyBill, FaHandHolding, FaUserCircle } from 'react-icons/fa';
+
 import Home from './Home'
 import Buy from './Buy'
 import Sell from './Sell'
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
 
 export const Nav = () => (
   <Router>
     <div className="flex flex-row">
       <div className="w-1/4 h-screen bg-indigo-darker">
         <Link className="text-4xl font-bold text-white no-underline" to="/">
-          <p className="mb-6 text-left pl-6 py-3 bg-white text-indigo-darker">
+          <p className="mb-6 text-left pl-6 py-3 bg-indigo text-white">
             PBR
           </p>
         </Link>
-        <Link className="text-xl text-indigo-lighter hover:text-white no-underline" to="/">
-          <p className="py-6 hover:bg-indigo-dark text-left pl-6">
+        <Link className="pl-6 flex flex-row items-center text-xl text-indigo-lighter hover:text-white no-underline" to="/">
+          <FaTachometerAlt />
+
+          <p className="py-6 text-left pl-6">
             Dashboard
             </p>
         </Link>
-        <Link className="text-xl text-indigo-lighter hover:text-white no-underline" to="/profile">
-          <p className="py-6 hover:bg-indigo-dark text-left pl-6">
+        <Link className="pl-6 flex flex-row items-center text-xl text-indigo-lighter hover:text-white no-underline" to="/profile">
+          <FaUserCircle />
+
+          <p className="py-6 text-left pl-6">
             Profile
             </p>
         </Link>
-        <Link className="text-xl text-indigo-lighter hover:text-white no-underline" to="/buy">
-          <p className="py-6 hover:bg-indigo-dark text-left pl-6">
+        <Link className="pl-6 flex flex-row items-center justify-start text-xl text-indigo-lighter hover:text-white no-underline" to="/buy">
+          <FaMoneyBill />
+          <p className="py-6 text-left pl-6">
             Buy
             </p>
         </Link>
-        <Link className="text-xl text-indigo-lighter hover:text-white no-underline" to="/sell">
-          <p className="py-6 hover:bg-indigo-dark text-left pl-6">
+        <Link className="pl-6 flex flex-row items-center text-xl text-indigo-lighter hover:text-white no-underline" to="/sell">
+          <FaHandHolding />
+          <p className="py-6 text-left pl-6">
             Sell
             </p>
         </Link>
